d4bb270aeb0efebed44105110b6971383e13cad3
1484-gbs <naoki@gbservice.co.jp>
2023-03-14 16:32:11 +0900

    modify csv download

--- api/src/main/kotlin/com/example/demo/usecase/GetEmployeeListCSVUseCase.kt
+++ api/src/main/kotlin/com/example/demo/usecase/GetEmployeeListCSVUseCase.kt
@@ -1,11 +1,12 @@
 package com.example.demo.usecase
 
+import com.example.demo.csv.CustomHeaderColumnNameMappingStrategyBuilder
 import com.example.demo.dto.EmployeeListCSV
 import com.example.demo.repository.EmployeeMapper
 import com.example.demo.type.GenderType
 import com.opencsv.CSVWriter
-import com.opencsv.bean.*
-import org.apache.commons.lang3.reflect.FieldUtils
+import com.opencsv.bean.StatefulBeanToCsv
+import com.opencsv.bean.StatefulBeanToCsvBuilder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.io.ByteArrayOutputStream
@@ -36,23 +37,10 @@ class GetEmployeeListCSVUseCaseCaseImpl(
                     val beanToCsv: StatefulBeanToCsv<EmployeeListCSV> =
                         StatefulBeanToCsvBuilder<EmployeeListCSV>(cw)
                             .withMappingStrategy(
-                                HeaderColumnNameMappingStrategy<EmployeeListCSV>()
-                                    .apply {
-                                        type = EmployeeListCSV::class.java
-                                        val headers = FieldUtils.getAllFields(type)
-                                            // FieldをCsvBindByPosition.position順で並び替え
-                                            .sortedWith(
-                                                compareBy {
-                                                    it.getAnnotation(CsvBindByPosition::class.java)?.position
-                                                }
-                                            )
-                                            .mapNotNull {
-                                                it.getAnnotation(CsvBindByName::class.java)?.column
-                                            }
-                                        setColumnOrderOnWrite(compareBy { headers.indexOf(it) })
-                                    })
-                            .build();
-                    beanToCsv.write(employees);
+                                CustomHeaderColumnNameMappingStrategyBuilder().build()
+                            )
+                            .build()
+                    beanToCsv.write(employees)
                 }
             }
             return baos.toByteArray()
